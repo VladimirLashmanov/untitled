a851ecaa985b75771098e5829e54ba03f6242202
VladimirLashmanov <vov7las@gmail.com>
2022-11-15 16:50:41 +0200

    add simp first

---
+++ calcul.js
@@ -0,0 +1,67 @@
+
+
+
+//
+// function calculateProfit(amount=1000, percent=5, period=2) {
+//
+//     let sum = 0;
+//     let amount2 = amount;
+//
+//
+//     for (let i = 1; i <= period; i += 1) {
+//         sum += amount2 * percent / 100;
+//         amount2 += amount2 * percent / 100;
+//     };
+
+//     return sum;
+//  }
+// calculateProfit();
+//
+// const title = 'Mate academy';
+// console.log(title);
+// console.log(title.length);
+// console.log(title.length-4);
+// console.log(title[title.length-4]);
+// console.log(title.at(-2));
+//
+// function isLetter(ch) {
+//     return ch.toLowerCase() !== ch.toUpperCase();
+// }
+//
+// console.log(ch);
+//
+//
+// function getStringLength(input) {
+//     return input.split("").length;
+// }
+//
+// function getStringLength(input) {
+//     return Object.values(input).length;
+// }
+
+    //
+    // function isWerewolf(target) {
+    //     const direct =target;
+    //     let reversed ='';
+    //     for (let i = target.length-1;i >= 0;i--){
+    //         reversed+=direct[i];
+    //     }
+    //
+    //
+    //
+    //  console.log(isWerewolf('rotator')); //=== true // rotator --> rotator
+    //  console.log(isWerewolf('home')); //=== false // home --> emoh
+    //  console.log(isWerewolf('Racecar')); //=== true // регістр ігнорується
+    //  console.log(isWerewolf('eva, can i see bees in a cave')); //=== true
+    //
+    //
+
+
+
+
+
+
+
+
+
+
